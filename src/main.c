100917164a086407b52be069ab8ac96f88c02c68
Rudra Kumar <dj0rudra@gmail.com>
2026-08-16 00:51:42 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,14 +5,15 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
+  while (true)
+  {
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
 
   char com[1024];
   scanf("%s",com);
   printf("%s: command not found\r\n", com);
-
+  }
 
   return 0;
 }
