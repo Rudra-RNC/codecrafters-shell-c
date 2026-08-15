76af3c8cc97cd8949dcec2a74e19ae23bd8fdecc
Rudra Kumar <dj0rudra@gmail.com>
2026-08-16 01:23:48 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,28 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void trim(char *s){
-  int i=0,j=0;
-  while(s[i]==' ') i++;
-  while(s[j++]=s[i++]);
+void trim(char *s)
+{
+  int i = 0, j = 0;
+  while (s[i] == ' ')
+    i++;
+  while (s[j++] = s[i++])
+    ;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   setbuf(stdout, NULL);
   while (1)
   {
-  printf("$ ");
+    printf("$ ");
 
-  char com[1024];
-  fgets(com,1024,stdin);
-  com[strlen(com)-1]='\0';
-  if(strcmp(com,"exit")==0) break;
-  else if(!strcmp(com,"echo")) printf("%s\n",com+5);
-  else if (strcmp(com,"type")==0)
-  {
-    if(strcmp(com+4,"echo")==0 || strcmp(com+4,"exit")==0 || strcmp(com+4,"type")==0) printf("%s is a shell builtin\n",com+4);
-    else printf("%s: command not found\n",com+4);
-  } else printf("%s: command not found\r\n", com);
+    char com[1024];
+    fgets(com, 1024, stdin);
+    com[strlen(com) - 1] = '\0';
+    if (strcmp(com, "exit") == 0)
+      break;
+    else if (!strcmp(com, "echo"))
+      printf("%s\n", com + 5);
+    else if (strcmp(com, "type") == 0)
+    {
+      if (strcmp(com + 4, "echo") == 0 || strcmp(com + 4, "exit") == 0 || strcmp(com + 4, "type") == 0)
+        printf("%s is a shell builtin\n", com + 4);
+      else
+        printf("%s: command not found\n", com + 4);
+    }
+    else
+      printf("%s: command not found\r\n", com);
+  }
 
   return 0;
 }
